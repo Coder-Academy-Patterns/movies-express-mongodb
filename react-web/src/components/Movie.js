7296f544be38d08e78e3fb901d82ff8fb554c7a4
Patrick Smith <patrick@burntcaramel.com>
2017-06-20 09:53:18 +1000

    Add React Web app

---
+++ react-web/src/components/Movie.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Movie({
+    title,
+    yearReleased
+}) {
+    return (
+        <div>
+            <h2>{ title }</h2>
+        </div>
+    )
+}
