0996e96af41b70c29b6414adc9b3384b0d9fcfe8
Patrick Smith <patrick@burntcaramel.com>
2017-06-27 11:23:04 +1000

    API: Add cors support

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const authMiddleware = require('./middleware/auth')
 const moviesRouter = require('./routes/movies')
 const peopleRouter = require('./routes/people')
@@ -13,6 +14,10 @@ const server = express()
 server.use(bodyParser.json())
 // Connect Passport to Express
 server.use(authMiddleware.initialize)
+// CORS
+server.use(cors({
+    origin: process.env.CORS_ORIGINS.split(',')
+}))
 
 // Routes
 server.use(moviesRouter)
