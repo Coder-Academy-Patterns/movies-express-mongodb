37dbb16be97e1da2125103c99fc5096424d09664
Patrick Smith <patrick@burntcaramel.com>
2017-06-27 11:58:10 +1000

    Fix merge conflict

--- react-web/src/api/movies.js
+++ react-web/src/api/movies.js
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
 import api from './init'
 
-// Read: list
 export function list() {
-    return api.get('/movies')
-        .then(res => res.data)
-=======
-import axios from './init'
-
-export function list() {
-    return axios.get('/movies').then(res => res.data)
+    return api.get('/movies').then(res => res.data)
 }
 
 export function create({ title, yearReleased, description }) {
-    return axios.post('/movies', {
+    return api.post('/movies', {
         title, yearReleased, description
     }).then(res => res.data)
->>>>>>> fa54ddc6523ce569354107605c38b75db3326eb4
 }
